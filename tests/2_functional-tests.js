b96efc83873988b2c8030c1f322857432220cbc2
Glitch (longhaired-cap) <none>
2019-12-27 08:52:10 +0000

    💨📈 Checkpoint ./tests/2_functional-tests.js:9298160/11947

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -33,7 +33,16 @@ suite('Functional Tests', function() {
     suite('POST /api/books with title => create book object/expect book object', function() {
       
       test('Test POST /api/books with title', function(done) {
-        //done();
+     chai.request(server)
+      .post('/api/books')
+      .end(function(err, res){
+        assert.equal(res.status, 200);
+        assert.isArray(res.body, 'response should be an array');
+        assert.property(res.body[0], 'commentcount', 'Books in array should contain commentcount');
+        assert.property(res.body[0], 'title', 'Books in array should contain title');
+        assert.property(res.body[0], '_id', 'Books in array should contain _id');
+        done();
+      });
       });
       
       test('Test POST /api/books with no title given', function(done) {
